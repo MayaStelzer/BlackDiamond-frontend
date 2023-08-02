dcec267d47dedfe81f1ddc28a55774d709445520
MayaStelzer <maya@bipper.net>
2023-08-01 19:43:37 -0700

    added fun fact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Card, { CardProps } from '../../components/cards'
 import "./styles.css"
 import {useState, useEffect} from 'react';
 
-
-
 /*
 const cardArray = [
     {
@@ -54,19 +52,24 @@ export default function Home() {
                <nav>
                    <ul>
                        <li>
+                          <input type="text" name="" id="search"/>
                            <i className="fa-solid fa-magnifying-glass"></i>
                            <a href="#home"></a>
-                           <input type="text" name="" id="search"/>
                        </li>
                    </ul>
                </nav>
            </div>
-    
-                <div className="content">
+                {/* <div className="content">
                     <h1>Maya's Awesome Skiing Site</h1>
                     <span>Maya's Awesome Skiing Site</span>
-                </div>
+                </div> */}
         </header>
+                <div id="funfact" className="funfact">
+                  <h3> <center> Fun Fact: The national ski patrol (NSP) was founded in 1938 by Charles Minot Dole in Stowe, Vermont. A big thank you to all the ski patrollers out there for your service and for keeping our ski-areas safe! </center></h3>
+                </div>
+        <section>
+
+        </section>
         <section id="home-articles" className="py-2">
             <div className="container">
             <div className="articles-container">
